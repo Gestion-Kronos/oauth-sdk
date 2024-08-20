21d9db1b15b1f55167200bd89dfad68ab4d50696
michael-gk <139917344+michael-gk@users.noreply.github.com>
2024-08-20 10:06:54 -0400

    WIP - Master

--- src/Auth/GKOAuthClient.php
+++ src/Auth/GKOAuthClient.php
@@ -9,29 +9,31 @@ class GKOAuthClient
 {
     public function __construct(
         protected string $client_id,
-        protected AuthFlow $auth_flow,
         protected string $pool_id,
         protected string $callback_uri,
     ) {}
 
     public function InitiateAuth(
         string $secret_hash,
-        GrantType $grant_type,
+        string $grant_type,
+        string $auth_flow,
         string|null $username = null,
         string|null $password = null
     ) {
         $auth_parameters = [
             "client_id" => $this->client_id,
             "pool_id" => $this->pool_id,
-            "auth_flow" => $this->auth_flow->value,
+            "auth_flow" => $auth_flow,
             "callback_uri" => $this->callback_uri,
             "auth_parameters" => [
                 "secret_hash" => $secret_hash,
-                "grant_type" => $grant_type->value,
+                "grant_type" => $grant_type,
+                "username" => $username,
+                "password" => $password,
             ]
         ];
 
-        if ($this->auth_flow === AuthFlow::USER_PASSWORD) {
+        if ($auth_flow === AuthFlow::USER_PASSWORD) {
             if (!$password || !$username) {
                 return json_encode([
                     'message' => 'INVALID_OR_MISSING_FIELDS',
@@ -46,7 +48,7 @@ class GKOAuthClient
         $curl = curl_init();
 
         curl_setopt_array($curl, [
-            CURLOPT_URL => 'https://auth.gestionkronos.com/api/initiate-auth',
+            CURLOPT_URL => 'http://127.0.0.1:8000/api/initiate-auth',
             CURLOPT_RETURNTRANSFER => true,
             CURLOPT_ENCODING => '',
             CURLOPT_MAXREDIRS => 100,
@@ -73,20 +75,21 @@ class GKOAuthClient
 
     public function AuthChallenge(
         string $secret_hash,
-        GrantType $grant_type,
+        string $grant_type,
         string $challenge_response,
+        string $auth_flow,
         string $username,
-        string $access_token
+        string $access_token,
+        string $target_user_id = null
     ) {
-
         $auth_parameters = [
             "client_id" => $this->client_id,
             "pool_id" => $this->pool_id,
-            "auth_flow" => $this->auth_flow->value,
+            "auth_flow" => $auth_flow,
             "callback_uri" => $this->callback_uri,
             "auth_parameters" => [
                 "secret_hash" => $secret_hash,
-                "grant_type" => $grant_type->value,
+                "grant_type" => $grant_type,
                 "challenge_response" => $challenge_response,
                 "username" => $username,
             ]
@@ -95,7 +98,7 @@ class GKOAuthClient
         $curl = curl_init();
 
         curl_setopt_array($curl, [
-            CURLOPT_URL => 'https://auth.gestionkronos.com/api/auth-challenge',
+            CURLOPT_URL => 'http://127.0.0.1:8000/api/auth-challenge',
             CURLOPT_RETURNTRANSFER => true,
             CURLOPT_ENCODING => '',
             CURLOPT_MAXREDIRS => 100,
@@ -105,20 +108,20 @@ class GKOAuthClient
             CURLOPT_POSTFIELDS => http_build_query($auth_parameters),
             CURLOPT_HTTPHEADER => [
                 'Accept: application/json',
-                'Authorization' => 'Bearer ' . $access_token
-
+                'Authorization: Bearer ' . $access_token
             ],
         ]);
 
         $response = curl_exec($curl);
+
         $err = curl_error($curl);
 
         curl_close($curl);
 
         if ($err) {
-            json_encode([
-               "message" => "CURL_ERROR",
-               "description" => "cURL Error #:" . $err
+            return json_encode([
+                "message" => "CURL_ERROR",
+                "description" => "cURL Error #:" . $err
             ]);
         }
 
@@ -127,21 +130,21 @@ class GKOAuthClient
         if (isset($response_data["error"])) {
             if (str_contains(strtolower($response_data["error"]), "totp")) {
                 return json_encode([
-                   "message" => "INVALID_TOTP_CODE",
-                   "description" => "The TOTP code given is either expired or invalid. Please try again.",
+                    "message" => "INVALID_TOTP_CODE",
+                    "description" => "The TOTP code given is either expired or invalid. Please try again.",
                 ]);
             }
 
             return json_encode([
                 "message" => "AUTHENTICATION_FAILED",
-                "description" => "An error occured while attempting to authenticate",
+                "description" => "An error occurred while attempting to authenticate",
             ]);
         }
 
         setcookie(
-          $response_data["token"]["access_token"]["type"],
-          $response_data["token"]["access_token"],
-          $response_data["token"]["duration"] / 60,
+            $response_data["token"]["access_token"]["type"],
+            $response_data["token"]["access_token"]["token"],
+            $response_data["token"]["access_token"]["duration"] / 60,
             '/',
             parse_url($this->callback_uri, PHP_URL_HOST),
             true,
