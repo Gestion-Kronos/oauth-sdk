c358012819d0153ff12e260994152d7ea0f90532
michael-gk <139917344+michael-gk@users.noreply.github.com>
2024-08-22 17:00:18 -0400

    WIP - Master

--- src/Auth/GKOAuthClient.php
+++ src/Auth/GKOAuthClient.php
@@ -4,7 +4,6 @@ namespace GestionKronos\Oauth\Auth;
 
 use GestionKronos\Oauth\Auth\Entities\AuthFlow;
 use GestionKronos\Oauth\Auth\Entities\GrantType;
-use GestionKronos\Oauth\Auth\Helpers\Helper;
 
 class GKOAuthClient
 {
@@ -17,22 +16,33 @@ class GKOAuthClient
     public function InitiateAuth(
         string $secret_hash,
         string $grant_type,
-        string $auth_flow,
+        AuthFlow $auth_flow,
         string|null $username = null,
-        string|null $password = null
+        string|null $password = null,
+        string $access_token = null,
     ) {
         $auth_parameters = [
             "client_id" => $this->client_id,
             "pool_id" => $this->pool_id,
-            "auth_flow" => $auth_flow,
+            "auth_flow" => $auth_flow->value,
             "callback_uri" => $this->callback_uri,
             "auth_parameters" => [
                 "secret_hash" => $secret_hash,
                 "grant_type" => $grant_type,
-                "username" => $username,
-                "password" => $password,
             ]
         ];
+
+        if ($auth_flow === AuthFlow::IMPERSONATE) {
+            if (!$username) {
+                return json_encode([
+                    'message' => 'INVALID_OR_MISSING_FIELDS',
+                    'description' => 'Invalid or missing form data. Please refer to api documentation for further instructions',
+                ]);
+            }
+
+            $auth_parameters['auth_parameters']['username'] = $username;
+        }
+
 
         if ($auth_flow === AuthFlow::USER_PASSWORD) {
             if (!$password || !$username) {
@@ -46,9 +56,10 @@ class GKOAuthClient
             $auth_parameters['auth_parameters']['username'] = $username;
         }
 
+
         $curl = curl_init();
 
-        curl_setopt_array($curl, [
+        $curl_params = [
             CURLOPT_URL => 'http://127.0.0.1:8000/api/initiate-auth',
             CURLOPT_RETURNTRANSFER => true,
             CURLOPT_ENCODING => '',
@@ -56,14 +67,24 @@ class GKOAuthClient
             CURLOPT_TIMEOUT => 300,
             CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
             CURLOPT_CUSTOMREQUEST => 'POST',
-            CURLOPT_POSTFIELDS => http_build_query($auth_parameters),
-            CURLOPT_HTTPHEADER => [
-                'Accept: application/json'
-            ],
-        ]);
+            CURLOPT_POSTFIELDS => http_build_query($auth_parameters)
+        ];
+
+        if ($auth_flow === AuthFlow::IMPERSONATE) {
+            $curl_params[CURLOPT_HTTPHEADER] = [
+                'Accept: application/json',
+                'Authorization: Bearer ' . $access_token
+            ];
+        }
+
+        curl_setopt_array($curl, $curl_params);
 
         $response = curl_exec($curl);
         $err = curl_error($curl);
+
+        if ($auth_flow === AuthFlow::IMPERSONATE) {
+            \Log::error(json_encode($response));
+        }
 
         curl_close($curl);
 
@@ -78,15 +99,14 @@ class GKOAuthClient
         string $secret_hash,
         string $grant_type,
         string $challenge_response,
-        string $auth_flow,
+        AuthFlow $auth_flow,
         string $username,
         string $access_token,
-        string $target_user_id = null
     ) {
         $auth_parameters = [
             "client_id" => $this->client_id,
             "pool_id" => $this->pool_id,
-            "auth_flow" => $auth_flow,
+            "auth_flow" => $auth_flow->value,
             "callback_uri" => $this->callback_uri,
             "auth_parameters" => [
                 "secret_hash" => $secret_hash,
@@ -142,22 +162,7 @@ class GKOAuthClient
             ]);
         }
 
-        setcookie(
-            $response_data["token"]["access_token"]["type"],
-            $response_data["token"]["access_token"]["token"],
-            $response_data["token"]["access_token"]["duration"] / 60,
-            '/',
-            parse_url($this->callback_uri, PHP_URL_HOST),
-            true,
-            true
-        );
-
-        setcookie('X-AUTH-CHALLENGE', '', time() - 3600, '/');
-
-        return json_encode([
-            'message' => 'AUTHENTICATION_SUCCESS',
-            'description' => 'You have been successfully logged in. Welcome Back!',
-        ]);
+        return $response_data;
     }
 
     public function InitiatePasswordReset(
@@ -187,63 +192,6 @@ class GKOAuthClient
 
         curl_setopt_array($curl, [
             CURLOPT_URL => 'http://127.0.0.1:8000/api/initiate-password-reset',
-            CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_ENCODING => '',
-            CURLOPT_MAXREDIRS => 100,
-            CURLOPT_TIMEOUT => 300,
-            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-            CURLOPT_CUSTOMREQUEST => 'POST',
-            CURLOPT_POSTFIELDS => http_build_query($auth_parameters),
-            CURLOPT_HTTPHEADER => [
-                'Accept: application/json'
-            ],
-        ]);
-
-        $response = curl_exec($curl);
-        $err = curl_error($curl);
-
-        curl_close($curl);
-
-        if ($err) {
-            return "cURL Error #: " . $err;
-        } else {
-            return json_decode($response, true);
-        }
-    }
-
-    public function ManageDataEncryption(
-        string $secret_hash,
-        string $auth_flow,
-        string $data,
-        string|null $username = null,
-    ) {
-        $auth_parameters = [
-            "client_id" => $this->client_id,
-            "pool_id" => $this->pool_id,
-            "auth_flow" => $auth_flow,
-            "callback_uri" => $this->callback_uri,
-            "data" => $data,
-            "auth_parameters" => [
-                "secret_hash" => $secret_hash,
-                "username" => $username,
-            ]
-        ];
-
-        $route_prefix = Helper::GenerateRoutePrefixSecretHash(
-            $username,
-            $this->client_id,
-            $this->pool_id
-        );
-
-        $route = Helper::GenerateRouteSecretHash(
-            $username,
-            $this->pool_id
-        );
-
-        $curl = curl_init();
-
-        curl_setopt_array($curl, [
-            CURLOPT_URL => `http://127.0.0.1:8000/api/{$route_prefix}/{$route}`,
             CURLOPT_RETURNTRANSFER => true,
             CURLOPT_ENCODING => '',
             CURLOPT_MAXREDIRS => 100,
